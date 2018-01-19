92f8b24fc52b599d698d63ed3422149272275a73
Javier Machin <javier_machin_acosta@hotmail.com>
2018-01-19 21:33:19 +0000

    Add gallery zoom functionality

--- scripts/galleryScript.js
+++ scripts/galleryScript.js
@@ -1,12 +1,14 @@
-let images = ["./images/small/elgolfo1s.jpg", "./images/small/timanfaya1s.jpg", "./images/small/timanfaya2s.jpg", "./images/small/timanfaya3s.jpg",
-              "./images/small/lagraciosas.jpg", "./images/small/lagraciosa2s.jpg", "./images/small/cuevadelosverdes1s.jpg", "./images/small/jameosdelagua1s.jpg", 
-              "./images/small/timanfaya4s.jpg", "./images/small/mar1s.jpg", "./images/small/cesarmanrique1s.jpg", "./images/small/cuevadelosverdes2s.jpg", 
-              "./images/small/jameosdelagua2s.jpg", "./images/small/jardindecactus1s.jpg", "./images/small/jardindecactus2s.jpg", "./images/small/miradorharia1s.jpg", 
-              "./images/small/timanfaya5s.jpg", "./images/small/timanfaya6s.jpg", "./images/small/tinguaton1s.jpg", "./images/small/PlayaDoradas.jpg"]
+let images = ["./images/small/elgolfo1.jpg", "./images/small/timanfaya1.jpg", "./images/small/timanfaya2.jpg", "./images/small/timanfaya3.jpg",
+              "./images/small/lagraciosa.jpg", "./images/small/lagraciosa2.jpg", "./images/small/cuevadelosverdes1.jpg", "./images/small/jameosdelagua1.jpg", 
+              "./images/small/timanfaya4.jpg", "./images/small/mar1.jpg", "./images/small/cesarmanrique1.jpg", "./images/small/cuevadelosverdes2.jpg", 
+              "./images/small/jameosdelagua2.jpg", "./images/small/jardindecactus1.jpg", "./images/small/jardindecactus2.jpg", "./images/small/miradorharia1.jpg", 
+              "./images/small/timanfaya5.jpg", "./images/small/timanfaya6.jpg", "./images/small/tinguaton1.jpg", "./images/small/PlayaDorada.jpg"]
 images.sort()
-
+let imageZoomWindow = document.getElementById('imageZoomWindow');
+let imageZoom = document.getElementById("imageZoom");
 let galleryGrid = document.querySelector(".gridContainer");
 
+// Creates as many divs as images there is to show in the gallery
 for (let i = 0; i < images.length; i++) {
 	let newDiv = document.createElement("div");
 	newDiv.classList.add('gridItem');
@@ -15,5 +17,21 @@ for (let i = 0; i < images.length; i++) {
 } 
 
 galleryGrid.style.cssText = 'grid-template-columns: repeat(3, 280px);' + 'grid-template-rows: repeat(7, 160px);';
+galleryGrid.addEventListener("click", fullSizeImage);
+
+function fullSizeImage(e) {
+	// Gets the source for the zoomed image by removing part of the route
+	// to the small version, directing it to the full size version
+	let imgSource = e.target.style.background;
+	imgSource = imgSource.replace("small/", "").split('"')[1];
+	if (imgSource.includes("images")) {
+		imageZoomWindow.style.display = "block";
+ 		imageZoom.src = imgSource;
+ 	}
+}
+let span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+  imageZoomWindow.style.display = "none";
+}
 
 
