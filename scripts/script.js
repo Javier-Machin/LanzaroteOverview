a5f40bcb0f773375bd34f2768deb346c61336c5c
Javier Machin <javier_machin_acosta@hotmail.com>
2018-01-20 21:22:07 +0000

    Fix language and header alignment

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,8 @@ function switchImages() {
 }
 
 function changeSource() {
-	let mySrc = topImg[0].getAttribute('src');
-	let mySrc2 = topImg[1].getAttribute('src');
-	if(mySrc === 'images/timanfaya1.jpg') {
+	let imgSrc1 = topImg[0].getAttribute('src');
+	if(imgSrc1 === 'images/timanfaya1.jpg') {
 	  topImg[0].setAttribute ('src', 'images/elgolfo1.jpg');
 	  topImg[1].setAttribute ('src', 'images/lagraciosa.jpg');
 	} else {
