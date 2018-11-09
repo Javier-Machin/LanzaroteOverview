0a25ebd9a4c2d51802067108b22b14cfbd6002ee
Javier Machin <javier_machin_acosta@hotmail.com>
2018-11-09 14:48:00 +0000

    minify css

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,18 @@ let topImg = document.querySelectorAll(".topImgContainer .contentImg");
 
 setInterval(switchImages, 11000);
 
+// Switch images on the home page
+
 function switchImages() {
 	topImg[0].classList.toggle("transparent");
 	topImg[1].classList.toggle("transparent");
-	setTimeout(changeSource, 1000);
+	
+  setTimeout(changeSource, 1000);
 }
 
 function changeSource() {
 	let imgSrc1 = topImg[0].getAttribute('src');
+
 	if(imgSrc1 === 'images/timanfaya1.jpg') {
 	  topImg[0].setAttribute ('src', 'images/elgolfo1.jpg');
 	  topImg[1].setAttribute ('src', 'images/lagraciosa.jpg');
@@ -18,6 +22,7 @@ function changeSource() {
 		topImg[0].setAttribute ('src', 'images/timanfaya1.jpg');
 		topImg[1].setAttribute ('src', 'images/timanfaya2.jpg');
 	}
+
 	setTimeout(function(){topImg[0].classList.toggle("transparent")}, 500);
 	setTimeout(function(){topImg[1].classList.toggle("transparent")}, 500);
 }
